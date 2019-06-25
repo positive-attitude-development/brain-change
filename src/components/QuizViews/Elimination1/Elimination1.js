4dc8ea670e08030b2d68420b53a2a043de54a9c2
Bobby Khounphinith <khoupe07@gmail.com>
2019-06-25 09:15:12 -0500

    Still working ongetting a value to select and deselect.

--- src/components/QuizViews/Elimination1/Elimination1.js
+++ src/components/QuizViews/Elimination1/Elimination1.js
@@ -1,32 +1,55 @@
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import React, { Component } from 'react'
-import { Link} from 'react-router-dom'; 
+import { Link } from 'react-router-dom'; 
 
 
 export class Elimination1 extends Component {
+
+    state = {
+        round1: [],
+        time: '',
+    }
+
+    handleNext = () => {
+        this.props.dispatch({ type: 'SET_NEW_VALUES', payload: this.state})
+        this.props.history.push('/ElimInstructions2')
+    }
+
+    handleSelect = (event) => {
+        // console.log('value is:', event.target.value)
+        for(let i=0; i<this.props.values.length; i++) {
+            console.log('value is:', event.target.value)
+
+            console.log('now checking value', this.props.values[i].values)
+            if(event.target.value === this.props.values[i].values) {
+                this.state.round1.splice(i, 1)
+                return;
+            }
+        } 
+        this.setState({
+            round1: [...this.state.round1, event.target.value]
+        })
+    }
 
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_VALUES'});
     }
 
     render() {
+        console.log(this.state.round1)
         return (
             <div>
-
-                <ul>
-                    {this.props.reduxState.valuesReducer.map(value => {
-                        return <li>{value.values}</li>
-                    })}
-                </ul>
-
-                
-
-
-                <Link to="/ElimInstructions2"> link </Link>
-
+                <div>
+                    <ul>
+                        {this.props.values.map(value => {
+                            return <li  onClick={this.handleSelect} value={value.values}>{value.values}</li>
+                        })}
+                    </ul>
+                </div>
+                {/* <Link to="/ElimInstructions2"> link </Link> */}
+                <div>
+                    <button onClick={this.handleNext}>Next</button>
+                </div>
             </div>
         )
     }
@@ -34,7 +57,7 @@ export class Elimination1 extends Component {
 
 const mapStateToProps = (reduxState) => {
     return {
-        reduxState
+        values: reduxState.valuesReducer
     }
 }
 
--- src/redux/reducers/valuesReducer.js
+++ src/redux/reducers/valuesReducer.js
@@ -1,6 +1,7 @@
 const valuesReducer = (state = [], action) => {
     switch(action.type) {
         case 'SET_VALUES':
+            console.log('Inside valuesReducer', action.payload)
             return action.payload;
         default:
             return state;
