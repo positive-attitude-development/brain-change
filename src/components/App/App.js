51756f3fa28f9357a6a9fb582facb6f771d6f412
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-06-25 12:00:58 -0500

    set up add participant area and myparticipants table

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -31,7 +31,6 @@ import {QuizViewWelcome} from '../QuizViews/QuizViewWelcome/QuizViewWelcome';
 class App extends Component {
   componentDidMount () {
     this.props.dispatch({type: 'FETCH_ADMIN'})
-    this.props.dispatch({type: 'FETCH_PROFILE'})
   }
 
   render() {
