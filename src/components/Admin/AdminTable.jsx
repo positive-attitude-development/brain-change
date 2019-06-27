7f60b63bcdd288a4139734af5b24ebe5b3ea7473
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-06-27 14:03:55 -0500

    uninstalled and reinstalled react-redux

--- src/components/Admin/AdminTable.jsx
+++ src/components/Admin/AdminTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux';
 import PropTypes from 'prop-types';
 import {IconButton, MenuItem, Table, TableBody, TableCell, 
         TableHead, TablePagination, TableRow, TableSortLabel, TextField, 
