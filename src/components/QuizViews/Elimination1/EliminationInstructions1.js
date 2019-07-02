9c652810aceb3ac10ca84ad40adbb5dd23befa5d
Jesse Gjerde <jessegjerde89@gmail.com>
2019-07-02 15:36:19 -0500

    instructions page update

--- src/components/QuizViews/Elimination1/EliminationInstructions1.js
+++ src/components/QuizViews/Elimination1/EliminationInstructions1.js
@@ -1,49 +1,39 @@
 import React, { Component } from 'react'
-
-import { Grid, Button, Paper } from '@material-ui/core';
+import { Link } from 'react-router-dom'; 
+import {  Button } from '@material-ui/core';
 import StatusBar from '../StatusBar'; 
 
 import './EliminationInstructions1.css'
 
-class EliminationInstructions1 extends Component {
-
-
-// handleNext() {
-   
-// }
+export class EliminationInstructions1 extends Component {
 
     state = {
         statusBar : 1
     }
     render() {
         return (
-
-            <Paper>
-
-            <div className="instructionView1">
-
+            <div>
                 <StatusBar status={this.state.statusBar} />
-                    <h3 className="textInst">
-                        On the next screen there will be a list of values. Remove the 9 values that are the least important to you.
-                    </h3>
-              
+                <div className="textInst">
+                
+                On the next screen there will be a list of values. 
+                Remove the 9 values that are the least important to you.
+                
+                 </div>
 
                  <div className="giph">
 
                 </div>
                 
-                  <div className="button">
-                    <Button  
-                        color="primary"
-                        variant="contained"
-                        onClick={() => this.props.history.push('/Elim1')}
-                        >
-                        Next
-                    </Button> 
-                  </div>
-                </div>
-            </Paper>
-
+            <Link to="/Elim1"> 
+                <Button
+                    color="primary"
+                    variant="contained"
+                    >
+                    Next
+                </Button> 
+            </Link>
+            </div>
         )
     }
 }
