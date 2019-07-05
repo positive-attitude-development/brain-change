1fe2bb4c56a762f2c1ba3b2b2fb2b5d888d2a3d1
Thomas Roselyn <tgroselyn@gmail.com>
2019-07-05 13:36:01 -0500

    added confirm to delete participant

--- src/components/IndividualParticipant/IndividualParticipant.jsx
+++ src/components/IndividualParticipant/IndividualParticipant.jsx
@@ -4,6 +4,7 @@ import {Card, CardContent, Tooltip, Grid, TextField, Button, MenuItem, Dialog, D
 import {withStyles} from '@material-ui/core/styles';
 import {Chance} from 'chance';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import Swal from 'sweetalert2';
 import moment from 'moment';
 
 const styles = {
@@ -83,8 +84,23 @@ class IndividualParticipant extends Component{
 	};//end handleCancelEdit
 
 	handleDelete = () => {
-		this.props.dispatch({type: 'DELETE_PARTICIPANT', payload: this.props.individual[0]});
-		this.props.history.push('/myparticipants');
+		Swal.fire({
+			title: `Delete ${this.props.individual[0].first_name}?`,
+			text: 'This action cannot be undone.',
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Yes, Delete'
+		}).then((result) => {
+			if (result.value) {
+				this.props.dispatch({type: 'DELETE_PARTICIPANT', payload: this.props.individual[0]});
+				this.props.history.push('/myparticipants');
+				Swal.fire(
+					'Deleted!',
+					'',
+					'success'
+				)
+			}
+		})
 	}
 
 	handleInputChange = propertyName => event => {
