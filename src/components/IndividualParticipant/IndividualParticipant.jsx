cbd528f8e5b6969eccc85e53f73a19d28c852a6f
Thomas Roselyn <tgroselyn@gmail.com>
2019-07-06 18:06:43 -0500

    updated edit dialog on participant page

--- src/components/IndividualParticipant/IndividualParticipant.jsx
+++ src/components/IndividualParticipant/IndividualParticipant.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Card, CardContent, Tooltip, Grid, TextField, Button, MenuItem, Dialog, DialogActions, DialogTitle, DialogContent} from '@material-ui/core';
+import {Card, CardContent, Tooltip, TextField, Button, MenuItem, Dialog, DialogActions, DialogTitle, DialogContent} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
 import {Chance} from 'chance';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
@@ -112,7 +112,7 @@ class IndividualParticipant extends Component{
 		let urlButton;
 
 		if (expirationDate >= today) {
-			urlButton = <Tooltip title="Invite Link Isn't Expired!" placement="right"><Button variant="contained" color="primary" size="large">Generate New Link</Button></Tooltip>
+			urlButton = <Tooltip title="Invite Link Isn't Expired!" placement="right"><Button color="primary" size="large">Generate New Link</Button></Tooltip>
 		} else {
 			urlButton = <Button variant="contained" color="primary" size="large" onClick={() => this.generateLink(person.urlid)}>Generate New Link</Button>
 		}
@@ -165,6 +165,92 @@ class IndividualParticipant extends Component{
 								}
 							</div>
 
+							{/* DIALOG EDITABLE FIELDS:*/}
+
+							<Dialog open={this.state.isEditable} onClose={this.handleCancelEdit} disableBackdropClick={true}>
+
+								<DialogTitle>Editing Participant: {person.first_name} {person.last_name}
+								</DialogTitle>
+
+								<DialogContent>
+									<h5>General Information:</h5>
+									<TextField label="First Name:" onChange={this.handleInputChange('first_name')} defaultValue={person.first_name} className={classes.menu} />
+
+									<TextField label="Last Name:" onChange={this.handleInputChange('last_name')} defaultValue={person.last_name} className={classes.menu} />
+
+									<TextField label="Age:" type="number" onChange={this.handleInputChange('age')} defaultValue={person.age} className={classes.menu} />
+
+									<TextField label="Gender:" select className={classes.menu} onChange={this.handleInputChange('gender')} value={this.props.editParticipant.gender} >
+										<MenuItem value="M">Male</MenuItem>
+										<MenuItem value="F">Female</MenuItem>
+										<MenuItem value="Other">Other</MenuItem>
+										<MenuItem value="Prefer Not to Say">Prefer Not to Say</MenuItem>
+									</TextField>
+
+									<TextField label="Category:" select className={classes.menu} value={this.props.editParticipant.category_id} onChange={this.handleInputChange('category_id')} >
+										{this.props.category.map(category => {
+											return (
+												<MenuItem key={category.id} value={category.id}>{category.category}</MenuItem>
+											)
+										})}
+									</TextField>
+
+									<TextField select label="State:" value={this.props.editParticipant.state} onChange={this.handleInputChange('state')} className={classes.menu}>
+										{this.props.stateNames.map((stateName, i) => {
+											return (
+												<MenuItem key={i} value={stateName.abbr}>{stateName.full}</MenuItem>
+											)
+										})}
+									</TextField>
+
+									<TextField label="Email Address:" className={classes.menu} defaultValue={person.email} />
+
+									<TextField label="Phone Number:" className={classes.menu} defaultValue={person.phone_number} />
+
+									{person.category_id === 1 &&
+										<>
+											<h5>Offender Information:</h5>
+
+											<TextField label="System:" select margin="normal" className={classes.menu}
+												value={this.props.editParticipant.offender_system_id} onChange={this.handleInputChange('offender_system_id')}>
+												{this.props.system.map((system) => {
+													return (<MenuItem key={system.id} value={system.id}>{system.system}</MenuItem>)
+												})}
+											</TextField>
+
+											<TextField label="System ID#:" className={classes.menu} defaultValue={person.system_id} onChange={this.handleInputChange('system_id')} />
+
+											<TextField label="Population:" select margin="normal" className={classes.menu}
+												value={this.props.editParticipant.population_id} onChange={this.handleInputChange('population_id')}>
+												{this.props.population.map((population) => {
+													return (<MenuItem key={population.id} value={population.id}>{population.population}</MenuItem>)
+												})}
+											</TextField>
+
+											<TextField select margin="normal" className={classes.menu}
+												label="Felony?:" value={this.props.editParticipant.felon}
+												onChange={this.handleInputChange('felon')} >
+												<MenuItem value={true}>Yes</MenuItem>
+												<MenuItem value={false}>No</MenuItem>
+											</TextField>
+
+											<TextField select margin="normal" className={classes.menu}
+												label="Violent Crime?:" value={this.props.editParticipant.violent_offender}
+												onChange={this.handleInputChange('violent_offender')} >
+												<MenuItem value={true}>Yes</MenuItem>
+												<MenuItem value={false}>No</MenuItem>
+											</TextField>
+										</>
+									}
+			
+								</DialogContent>
+
+								<DialogActions>
+									<Button onClick={this.handleCancelEdit} color="primary" size="large">Cancel Edit</Button>
+									<Button onClick={this.saveChanges} variant="contained" color="primary" size="large">Save Changes</Button>
+								</DialogActions>
+
+							</Dialog>
 						</CardContent>
 					</Card>
 
