b9ba60a3f933081f382b3abdba0f2e82f631d413
Thomas Roselyn <tgroselyn@gmail.com>
2019-07-05 19:50:21 -0500

    quiz welcome styled

--- src/components/QuizViews/QuizViewWelcome/QuizViewWelcomeForm.js
+++ src/components/QuizViews/QuizViewWelcome/QuizViewWelcomeForm.js
@@ -8,14 +8,15 @@ const styles = {
 
 	},
 	menu: {
-		margin: 'auto',
-		marginLeft: '5px',
-		width: '120px',
-	},
-    text: {
-		margin: '10px',
-		width: '120px',
-	},
+        marginTop: '5px',
+        marginLeft: '5px',
+        width: '150px',
+    },
+    menuXL: {
+        marginTop: '5px',
+        marginLeft: '5px',
+        width: '250px',
+    },
 }
 
 class QuizViewWelcomeForm extends Component {
@@ -49,97 +50,85 @@ class QuizViewWelcomeForm extends Component {
     }
 
     render() {
-        const classes = this.props;
-        console.log(this.state);
+        const {classes} = this.props;
+
         return (
-            <div>
-                <div id="registrationForm">
-                    <div>
-                    <TextField
-                        required className={classes.text}
-                        margin="normal"
-                        label="First Name"
-                        value={this.state.first_name}
-                        onChange={this.handleInputChange('first_name')} >
-                    </TextField>
-                    <TextField
-                        required className={classes.text}
-                        margin="normal"
-                        label="Last Name"
-                        value={this.state.last_name}
-                        onChange={this.handleInputChange('last_name')} >
-                    </TextField>
-                    <TextField
-                        type="number" className={classes.text}
-                        margin="normal"
-                        label="Age"
-                        style={{width:100}}
-                        value={this.state.age}
-                        onChange={this.handleInputChange('age')} >
-                    </TextField>
-                </div>
-                    {/* <TextField
-                        margin="normal"
-                        label="Gender"
-                        value={this.state.gender}
-                        onChange={this.handleInputChange('gender')} >
-                    </TextField> */}
-                <div>
-                    <TextField label="Gender:" 
-                               select margin="normal" 
-                               onChange={this.handleInputChange('gender')} 
-                               value={this.state.gender} 
-                               className={classes.menu}
-                               style={{width:170}}>
-							<MenuItem value="M">Male</MenuItem>
-							<MenuItem value="F">Female</MenuItem>
-							<MenuItem value="Other">Other</MenuItem>
-							<MenuItem value="Prefer Not to Say">Prefer Not to Say</MenuItem></TextField>
-                    <TextField
-                        required
-                        select
-                        style={{width:100}}
-                        className={classes.menu}
-                        margin="normal"
-                        label="State"
-                        value={this.state.state}
-                        onChange={this.handleInputChange('state')} >
-                        {this.props.stateNames.map((stateName, i) => {
-                            return (
-                                <MenuItem key={i} value={stateName.abbr}>{stateName.full}</MenuItem>
-                            )
-                        })}
-                    </TextField>
-                    <TextField
-                        select className={classes.text}
-                        margin="normal"
-                        style={{width:300}}
-                        label="Which best describes you?"
-                        value={this.state.category_id}
-                        onChange={this.handleInputChange('category_id')} >
-                        <MenuItem value=""><em>Select Category</em></MenuItem>
-                        <MenuItem value={2}>General Public</MenuItem>
-                        <MenuItem value={3}>Student</MenuItem>
-                        <MenuItem value={4}>Other</MenuItem>
-                    </TextField>
-                </div>
-                    <TextField
-                        margin="normal" className={classes.text}
-                        label="Email Address"
-                        value={this.state.email_address}
-                        onChange={this.handleInputChange('email_address')} >
-                    </TextField>
-                    <TextField
-                        margin="normal" className={classes.text}
-                        label="Phone Number"
-                        value={this.state.phone_number}
-                        onChange={this.handleInputChange('phone_number')} >
-                    </TextField>
+            <div id="registrationForm">
+                <TextField
+                    required
+                    className={classes.menu}
+                    label="First Name"
+                    value={this.state.first_name}
+                    onChange={this.handleInputChange('first_name')} >
+                </TextField>
+                <TextField
+                    required
+                    className={classes.menu}
+                    label="Last Name"
+                    value={this.state.last_name}
+                    onChange={this.handleInputChange('last_name')} >
+                </TextField>
+                <TextField
+                    type="number"
+                    className={classes.menu}
+                    label="Age"
+                    value={this.state.age}
+                    onChange={this.handleInputChange('age')} >
+                </TextField>
+                <TextField
+                    required
+                    select
+                    label="Gender"
+                    className={classes.menu}
+                    value={this.state.gender}
+                    onChange={this.handleInputChange('gender')} > 
+                        <MenuItem value="M">Male</MenuItem>
+                        <MenuItem value="F">Female</MenuItem>
+                        <MenuItem value="Other">Other</MenuItem>
+                        <MenuItem value="Prefer Not to Say">Prefer Not to Say</MenuItem>
+                </TextField>
+                <TextField
+                    required
+                    select
+                    label="State"
+                    className={classes.menu}
+                    value={this.state.state}
+                    onChange={this.handleInputChange('state')} >
+                    {this.props.stateNames.map(stateName => {
+                        return (
+                            <MenuItem key={stateName.abbr} value={stateName.abbr}>{stateName.full}</MenuItem>
+                        )
+                    })}
+                </TextField>
+                <TextField
+                    label="Email Address"
+                    className={classes.menu}
+                    value={this.state.email_address}
+                    onChange={this.handleInputChange('email_address')} >
+                </TextField>
+                <TextField
+                    label="Phone Number"
+                    className={classes.menu}
+                    value={this.state.phone_number}
+                    onChange={this.handleInputChange('phone_number')} >
+                </TextField>
+                <TextField
+                    select
+                    required
+                    className={classes.menuXL}
+                    label="Which best describes you?"
+                    value={this.state.category_id}
+                    onChange={this.handleInputChange('category_id')} >
+                    <MenuItem value={2}>General Public</MenuItem>
+                    <MenuItem value={3}>Student</MenuItem>
+                    <MenuItem value={4}>Other</MenuItem>
+                </TextField>
+                <div id="buttonArea">
                     <Button
                         variant="contained"
                         color="primary"
-                        onClick={this.submitRegistration}
-                    >
+                        size="large"
+                        onClick={this.submitRegistration} >
                         Register and Start
                     </Button>
                 </div>
