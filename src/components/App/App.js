d514cd8296a82f0020f1659a7106d2f4d37c41f6
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-07-10 10:52:58 -0500

    cleaned up App.js, deleted AboutPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,7 @@ import {connect} from 'react-redux';
 
 import Nav from '../Nav/Nav';
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
-import AboutPage from '../AboutPage/AboutPage';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 import Profile from '../Profile/Profile';
@@ -30,19 +29,8 @@ class App extends Component {
         <div>
           <Nav />
           <Switch>
-          
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
-            <Route
-              exact path="/about"
-              component={AboutPage}/>
 
-            {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/home will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-            Even though it seems like they are different pages, the user is always on localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
             <ProtectedRoute
               exact path="/home"
@@ -52,8 +40,6 @@ class App extends Component {
               exact path="/allparticipants"
               component={AllRecords}/>
 
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
             <ProtectedRoute
               exact path="/info"
               component={InfoPage}/>
