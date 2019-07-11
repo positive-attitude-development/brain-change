6d545c4a5bf6c0e1849b0cb583aca9f9f550bfbe
PADgroup <52079029+PADgroup@users.noreply.github.com>
2019-07-11 16:22:28 -0500

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -10,7 +10,7 @@ if (process.env.DATABASE_URL) {
   const auth = params.auth.split(':');
 
   config = {
-    admin: auth[0],
+    user: auth[0],
     password: auth[1],
     host: params.hostname,
     port: params.port,
